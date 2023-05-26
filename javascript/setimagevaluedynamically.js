377e67cdc55178798e5f1543d38ccf9432423da8
vikasanjana <99138961+vikasanjana@users.noreply.github.com>
2023-05-26 12:37:35 +0530

    Update setimagevaluedynamically.js

--- javascript/setimagevaluedynamically.js
+++ javascript/setimagevaluedynamically.js
@@ -1,1 +1,24 @@
+const  setImageUrlToInput = (imageUrl) =>{
+   var inputElement = $('.pplr_monogram.fileupload')[0];
+   var xhr = new XMLHttpRequest();
+    xhr.open("GET", imageUrl, true);
+    xhr.responseType = "blob";
+    
+    xhr.onload = function () {
+      if (xhr.status === 200) {
+        var blob = xhr.response;
+    
+        // Create a File object from the downloaded image blob
+        var file = new File([blob], "image.jpg", { type: blob.type });
+    
+        // Create a new DataTransfer object and add the File object to it
+        var dataTransfer = new DataTransfer();
+        dataTransfer.items.add(file);
+    
+        // Set the files property of the file input element using the DataTransfer object
+        inputElement.files = dataTransfer.files;
+      }
+    };
 
+    xhr.send();
+}
