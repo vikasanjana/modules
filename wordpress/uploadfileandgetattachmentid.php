ef9e001cc7d5273d889b5b46b5cf424d590f011a
vikasanjana <99138961+vikasanjana@users.noreply.github.com>
2023-06-02 11:41:42 +0530

    Update uploadfileandgetattachmentid.php

--- wordpress/uploadfileandgetattachmentid.php
+++ wordpress/uploadfileandgetattachmentid.php
@@ -1,6 +1,6 @@
- /**
+/**
      * Function for uplaod logo file and get attachment id
-     * @param Array Files['file'] array getting from form
+     * @param Array only key should pass array getting from form
      * @return Int Attachement id
      */
     public function uf_uplaod_logo_file($file)
@@ -8,7 +8,7 @@
         require_once(ABSPATH . 'wp-admin/includes/image.php');
         require_once(ABSPATH . 'wp-admin/includes/file.php');
         require_once(ABSPATH . 'wp-admin/includes/media.php');
-        $attachment_id = media_handle_upload($file, 0);
+        $attachment_id = media_handle_upload('file', 0);
         if (!is_wp_error($attachment_id)) {
             return  $attachment_id;
         } else {
